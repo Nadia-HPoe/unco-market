0b076ede906909c063ed447ea001fc7d74509778
Nadia <heatherpoe.bn@gmail.com>
2025-06-07 17:47:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const track = document.querySelector('.slider__track');
-const slides = document.querySelectorAll('.slider__banner');
-const steps = document.querySelectorAll('.slider__step');
-const slider = document.querySelector('.slider');
+const track = document.querySelector(".slider__track");
+const slides = document.querySelectorAll(".slider__banner");
+const steps = document.querySelectorAll(".slider__step");
+const slider = document.querySelector(".slider");
+const burger = document.querySelector(
+  ".header__navigation-personal-burger-menu"
+);
+const menu = document.querySelector(".header__navigation-menu");
 
 let currentIndex = 0;
 const totalSlides = slides.length;
 let intervalId;
 
 function showSlide(index) {
-  steps.forEach((step, i) => step.classList.toggle('activeSlide', i === index));
-  slides.forEach((slide, i) => slide.classList.toggle('sawSlide', i === index)) 
+  steps.forEach((step, i) => step.classList.toggle("activeSlide", i === index));
+  slides.forEach((slide, i) => slide.classList.toggle("sawSlide", i === index));
 }
 
 function startSlider() {
@@ -23,15 +27,24 @@ function stopSlider() {
   clearInterval(intervalId);
 }
 
-slider.addEventListener('mouseenter', stopSlider);
-slider.addEventListener('mouseleave', startSlider);
+slider.addEventListener("mouseenter", stopSlider);
+slider.addEventListener("mouseleave", startSlider);
 
 showSlide(currentIndex);
 startSlider();
 
 steps.forEach((step, i) => {
-  step.addEventListener('click', () => {
+  step.addEventListener("click", () => {
     currentIndex = i;
     showSlide(currentIndex);
   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (burger) {
+    burger.addEventListener("click", function () {
+      burger.classList.toggle("active");
+      menu.classList.toggle("active");
+    });
+  }
 });
