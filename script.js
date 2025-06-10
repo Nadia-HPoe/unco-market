5fae64ee65b654072b3fe6924ae4728a6f40927f
Nadia <heatherpoe.bn@gmail.com>
2025-06-10 20:21:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const track = document.querySelector(".slider__track");
-const slides = document.querySelectorAll(".slider__banner");
-const steps = document.querySelectorAll(".slider__step");
-const slider = document.querySelector(".slider");
+const track = document.querySelector('.slider__track');
+const slides = document.querySelectorAll('.slider__banner');
+const steps = document.querySelectorAll('.slider__step');
+const slider = document.querySelector('.slider');
 const burger = document.querySelector(
   ".header__navigation-personal-burger-menu"
 );
 const menu = document.querySelector(".header__navigation-menu");
+const blocks = document.querySelectorAll('.block');
+const blocksArray = Array.from(blocks);
+const blockArrowLeft = document.getElementById("eco-system-arrow-left");
+const blockArrowRight = document.getElementById("eco-system-arrow-right");
 
 let currentIndex = 0;
 const totalSlides = slides.length;
 let intervalId;
+let blocksMaxOrder = 6;
+let blocksMinOrder = 0;
 
 function showSlide(index) {
   steps.forEach((step, i) => step.classList.toggle("activeSlide", i === index));
@@ -48,3 +54,36 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  blockArrowLeft.addEventListener("click", () => subBlockOrder())
+
+  blockArrowRight.addEventListener("click", () => addBlockOrder())
+
+  const addBlockOrder = () => {
+      [blocksMinOrder, blocksMaxOrder] = 
+        blocksMaxOrder === 17 ? [0, 6] :
+        blocksMaxOrder === 12 ? [12, 17] :
+        [6, 12];
+    createBlocks();
+  }
+
+  const subBlockOrder = () => {
+      [blocksMinOrder, blocksMaxOrder] = 
+        blocksMinOrder === 12 ? [6, 12] :
+        blocksMinOrder === 6 ? [0, 6] :
+        [12, 17];
+   createBlocks();
+  }
+
+  const createBlocks = () => {
+    blocksArray.forEach((block,index) => {
+      index >= blocksMinOrder && index < blocksMaxOrder 
+      ? 
+        block.style.display = "flex" 
+      : 
+        block.style.display = "none"
+    })
+  }
+})
